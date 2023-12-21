63193d6c3edae5ed0b0ddfa450418d212695fa8e
CHOPPER <isaac.sanchez8791@alumno.udg.mx>
2023-12-20 18:55:56 -0600

    completo sin next_prve

--- funciones/editar_funciones.js
+++ funciones/editar_funciones.js
@@ -1,10 +1,70 @@
-var queryParams = new URLSearchParams(window.location.search);
-var datosString = queryParams.get("datos");
-var datos = JSON.parse(decodeURIComponent(datosString));
-       console.log(datos);
+var datos_ideas=JSON.parse(localStorage.getItem('datos_ideas'));
+var datos_poemas=JSON.parse(localStorage.getItem('datos_poemas'));
 
+var poema_idea =  localStorage.getItem('poema_idea');
+var id = localStorage.getItem('id');
+
+function funcion() {
+  pagina_actual=0;
+
+  let elemento;
+  var fondo=document.getElementById("fondo_imagen");
+
+  var body = document.body;
+  var headr = document.getElementById("header");
+  var luna_sol=document.getElementById("sol_luna");
+
+
+  if (poema_idea=='true') {//NOCHE
+    
+    body.style.backgroundImage = "linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)";
+    fondo.style.backgroundImage="url(funciones/files/nubes.png)";
+    headr.style.backgroundColor="#9BE8D8";
+    luna_sol.style.backgroundImage="url(funciones/files/sol.png)";
+    elemento=1;
+
+
+  } else {//DIA
+    body.style.backgroundImage = "linear-gradient(99.9deg, rgba(27,24,31,1) 21.2%, rgba(50,4,89,1) 84.8%)";
+    fondo.style.backgroundImage="url(funciones/files/estrellas.png)";
+    headr.style.backgroundColor="#03506F";
+    luna_sol.style.backgroundImage="url(funciones/files/luna.png)";
+    elemento=0;
+
+
+
+  }
+
+  
+  }
+
+  function BL(id){
+    if(!poema_idea){
+      for(i=0;i<datos_ideas.length;i++){
+        if(datos_ideas[i].id==id){
+          return i;
+        }
+      }
+    }else{
+      for(i =0; i<datos_poemas.length;i++){
+        if(datos_poemas[i].id==id){
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
 window.onload = function() {
-  let form = document.getElementById("formulario");
+  let subtitulo =document.getElementById("subtitulo");
+  console.log(((poema_idea=='true')?("Idea"):("Poema")));
+  subtitulo.innerText=(poema_idea=='true')?("Idea"):("Poema")
+  funcion();
+  let posicion =BL(id);  
+  var datos={
+    titulo: (poema_idea=='true')?(JSON.parse(localStorage.getItem('datos_ideas'))[posicion].titulo):((JSON.parse(localStorage.getItem('datos_poemas'))[posicion].titulo)),
+    texto: (poema_idea=='true')?(JSON.parse(localStorage.getItem('datos_ideas'))[posicion].texto):((JSON.parse(localStorage.getItem('datos_poemas'))[posicion].texto)),
+  }
+
   let titulo = document.getElementById("nombre");
   let texto = document.getElementById("texto");
   titulo.setAttribute("value",datos.titulo);
@@ -15,18 +75,27 @@ function vacio(){
     $('#alert').html('Datos faltantes');
     setTimeout("$('#alert').html('');",3000);  
 }
-
-  function registro_edit(){
-    let form = document.getElementById("formulario");
-    let formData = new FormData(form);
-    
+function listo_editar(titulo,texto){
+  var datos;
+  (poema_idea=='true')?(datos=JSON.parse(localStorage.getItem('datos_ideas'))):(datos=JSON.parse(localStorage.getItem('datos_poemas')));
+  datos[id-1].titulo=titulo;
+  datos[id-1].texto=texto;
+  let t_foto=datos[id-1].imagen;
+  //alert(t_foto)
+  //t_foto==""?datos[id-1].imagen=datos[id-1].imagen:datos[id-1].imagen=imagen;
   
+  (poema_idea=='true')?(localStorage.setItem('datos_ideas', JSON.stringify(datos))):(localStorage.setItem('datos_poemas', JSON.stringify(datos)));
+
+  alert("Se ha editado correctamente"); 
+  window.location.replace("index.html");
+}
+function registro_edit(){
+
     let titulo = document.formulario.nombre.value;
     let texto = document.formulario.texto.value;
-    let imagen = document.formulario.imagen.value;
     (titulo=="")||(texto=="")?
     vacio()
-    :subir_editar(formData);
+    :listo_editar(titulo,texto);
     
   
   
---
+++ funciones/nuevo_funciones.js
@@ -0,0 +1,91 @@
+
+function subida(titulo,texto){
+    var prueba = document.getElementById("switch");
+    
+    if(prueba.checked){
+      localStorage.setItem('lastId_Idea', parseInt(localStorage.getItem('lastId_Idea'))+parseInt(1));
+
+        var datos = JSON.parse(localStorage.getItem('datos_ideas'));
+
+        datos.push({
+          id: parseInt(localStorage.getItem('lastId_Idea')),
+          titulo: titulo,
+            texto: texto,
+            imagen: 0,
+        });
+        localStorage.setItem('datos_ideas', JSON.stringify(datos));
+
+    }else{
+      localStorage.setItem('lastId_Poema', parseInt(localStorage.getItem('lastId_Poema'))+parseInt(1));
+
+        var datos = JSON.parse(localStorage.getItem('datos_poemas'));
+        datos.push({
+            id: parseInt(localStorage.getItem('lastId_Poema')),
+            titulo: titulo,
+            texto: texto,
+            imagen: 0,
+        });
+        localStorage.setItem('datos_poemas', JSON.stringify(datos));
+
+    }
+        alert("Se ha registrado correctamente"); 
+        window.location.replace("index.html");
+  }
+  
+  function registro_nuevo(){
+
+  
+  
+    let titulo = document.formulario.nombre.value;
+    let texto = document.formulario.texto.value;
+    (titulo=="")||(texto=="")?
+    vacio()
+    
+    :subida(titulo,texto);
+    
+  
+  
+  
+   return false;
+    
+  }
+  
+
+
+
+
+function funcion() {
+    pagina_actual=0;
+  
+    let elemento;
+    var prueba = document.getElementById("switch");
+    var fondo=document.getElementById("fondo_imagen");
+  
+    var body = document.body;
+    var headr = document.getElementById("header");
+    var luna_sol=document.getElementById("sol_luna");
+  
+  
+    if (prueba.checked) {//NOCHE
+      
+      body.style.backgroundImage = "linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)";
+      fondo.style.backgroundImage="url(funciones/files/nubes.png)";
+      headr.style.backgroundColor="#9BE8D8";
+      luna_sol.style.backgroundImage="url(funciones/files/sol.png)";
+      elemento=1;
+  
+  
+    } else {//DIA
+      body.style.backgroundImage = "linear-gradient(99.9deg, rgba(27,24,31,1) 21.2%, rgba(50,4,89,1) 84.8%)";
+      fondo.style.backgroundImage="url(funciones/files/estrellas.png)";
+      headr.style.backgroundColor="#03506F";
+      luna_sol.style.backgroundImage="url(funciones/files/luna.png)";
+      elemento=0;
+  
+  
+  
+    }
+  
+    
+    }
+  
